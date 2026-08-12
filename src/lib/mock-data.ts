a7de692efdb2c496968aed28e572bd60edb1bc8b
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-08-12 19:31:31 -0300

    feat: adiciona mock

---
+++ src/lib/mock-data.ts
@@ -0,0 +1,10 @@
+import { Admin } from "./types";
+
+export const admins: Admin[] = [
+  {
+    id: "1",
+    nome: "Admin",
+    email: "admin@beatmanager.com",
+    senha: "123456",
+  },
+];
---
+++ src/lib/types.ts
@@ -0,0 +1,6 @@
+export interface Admin {
+  id: string;
+  nome: string;
+  email: string;
+  senha: string;
+}
