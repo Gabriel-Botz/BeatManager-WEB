a4ed3899276679b0c085e0de5ae0144b9158d182
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-08-14 00:48:23 -0300

    feat: adiciona responsividade

--- src/app/eventos/meus/page.tsx
+++ src/app/eventos/meus/page.tsx
@@ -100,7 +100,7 @@ export default function MeusEventosPage() {
       <FundoEfeitoBrilho />
 
       <Cabecalho>
-        <img src="/logo-header2.png" alt="BeatManager" style={{ width: 210, height: 70, objectFit: "contain" }} />
+        <img src="/logo-header2.png" alt="BeatManager" className="logo-cabecalho" />
         <CabecalhoLogado aoSair={logout} />
       </Cabecalho>
 
